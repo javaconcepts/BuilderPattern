01574a22a0114f1f6d7afff35dbe9e8d777a3c00
Jai Pal <jaipal.saini.0895@gmail.com>
2024-07-01 18:12:19 +0530

    Components defined

--- components/Engine.java
+++ components/Engine.java
@@ -1,5 +1,43 @@
 package BuilderPattern.components;
 
 public class Engine {
-    
+
+    private final double volume;
+    private double mileage;
+    private boolean started;
+
+    public Engine(double vol, double mil) {
+        this.volume = vol;
+        this.mileage = mil;
+    }
+
+    public void on() {
+        started = true;
+    }
+
+    public void off() {
+        started = false;
+    }
+
+    public boolean isStarted() {
+        return started;
+    }
+
+    public void go(double mileage) {
+        if (started) {
+            this.mileage += mileage;
+        }
+        else{
+            System.err.println("Cannot go(), you must start engine first!");
+        }
+    }
+
+    public double getVolume(){
+        return volume;
+    }
+
+    public double getMileage(){
+        return mileage;
+    }
+
 }
