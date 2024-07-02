ff38b1bfef2beae1fa78b1ca908c5ad023880b5f
Jai Pal <jaipal.saini.0895@gmail.com>
2024-07-02 20:19:43 +0530

    client code implement

--- Demo.java
+++ Demo.java
@@ -1,9 +1,20 @@
 package BuilderPattern;
 
+import BuilderPattern.builders.CarBuilder;
+import BuilderPattern.cars.Car;
+import BuilderPattern.director.Director;
+
 public class Demo {
 
     public static void main(String[] args) {
+        Director director = new Director();
+        CarBuilder carBuilder = new CarBuilder();
 
+        director.constructSportsCar(carBuilder);
+
+        Car car = carBuilder.getResult();
+
+        System.out.println(car.getCarType());
     }
 
 }
--- director/Director.java
+++ director/Director.java
@@ -1,9 +1,26 @@
 package BuilderPattern.director;
 
-import java.lang.module.ModuleDescriptor.Builder;
+import BuilderPattern.cars.CarType;
+import BuilderPattern.components.Engine;
+import BuilderPattern.components.GPSNavigator;
+import BuilderPattern.components.Transmission;
+import BuilderPattern.components.TripComputer;
+import BuilderPattern.builders.Builder;
 
+/* 
+ * Director defines the order if building steps.
+ * It works with a builder object through common 
+ * builder interface. 
+ * Therefore it may not know what product is being 
+ * built.
+ */
 public class Director {
-    public void constructSportsCar(Builder builder){
-
+    public void constructSportsCar(Builder builder) {
+        builder.setCarType(CarType.SPORTS_CAR);
+        builder.setSeats(2);
+        builder.setEngine(new Engine(3.0, 0));
+        builder.setTransmission(Transmission.SEMI_AUTOMATIC);
+        builder.setTripComputer(new TripComputer());
+        builder.setGpsNavigator(new GPSNavigator());
     }
 }
